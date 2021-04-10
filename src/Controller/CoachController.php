9f4da47d47b5559b7d996232146b556ea3f551f6
kenzaABCD <kenza.ghenimi@gmail.com>
2021-04-10 22:52:51 +0100

    commit 3

--- src/Controller/CoachController.php
+++ src/Controller/CoachController.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace App\Controller;
+
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Request;
@@ -9,6 +10,7 @@ use App\Entity\Coach;
 use App\Form\CoachType;
 use App\Repository\CoachRepository;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+
 class CoachController extends AbstractController
 {
     /**
@@ -26,10 +28,10 @@ class CoachController extends AbstractController
     public function Affichage()
     {
         $repo = $this->getDoctrine()->getManager()->getRepository(Coach::class);
-        $Co = $repo->findAll();    
-        
+        $Co = $repo->findAll();
+
         return $this->render('coach/Affichage.html.twig', [
-            'coach' => $Co 
+            'coach' => $Co
         ]);
     }
     /**
@@ -37,18 +39,49 @@ class CoachController extends AbstractController
      */
     public function add(Request $request)
     {
-        $coach=new Coach();
-        $form=$this->createForm(CoachType::class,$coach);
-        $form->add('Ajouter',SubmitType::class);
+        $coach = new Coach();
+        $form = $this->createForm(CoachType::class, $coach);
+        $form->add('Ajouter', SubmitType::class);
         $form->handleRequest($request);
-               if ($form->isSubmitted() && $form->isValid()) {
-            $em= $this->getDoctrine()->getManager();
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
             $em->persist($coach);
             $em->flush();
             return $this->redirectToRoute('Affichage');
+        }
+        return $this->render('Coach/add.html.twig', [
+            'form' => $form->createView()
+        ]);
     }
-    return $this->render('Coach/add.html.twig',[
-        'form'=>$form->createView()
-    ]);
-}
+   
+    /**
+     * @Route("/update/{id}",name="update")
+     */
+    public function update(Request $request, $id, CoachRepository $repo)
+    {
+        $coach = $repo->find($id);
+        $editForm = $this->createForm(CoachType::class, $coach);
+        $editForm->add('Update', SubmitType::class);
+        $editForm->handleRequest($request);
+        if ($editForm->isSubmitted() && $editForm->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($coach);
+            $em->flush();
+            return $this->redirectToRoute('Affichage');
+        }
+        return $this->render('Coach/update.html.twig', [
+            'form' => $editForm->createView()
+        ]);
+    }
+     /**
+     * @Route("/delete/{id}",name="delete")
+     */
+    function delete($id, CoachRepository $repo)
+    {
+        $coach = $repo->find($id);
+        $em = $this->getDoctrine()->getManager();
+        $em->remove($coach);
+        $em->flush();
+        return $this->redirectToRoute('Affichage');
+    }
 }
--- src/Form/CoachType.php
+++ src/Form/CoachType.php
@@ -4,24 +4,43 @@ namespace App\Form;
 
 use App\Entity\Coach;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
 
 class CoachType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('nom')
-            ->add('prenom')
-            ->add('email')
+            ->add('nom', TextType::class,array(
+                'label'=>'Nom Coach',
+                'attr'=>[
+                    'placeholder'=>'Ben Foulen....'
+                ]
+            ))
+            ->add('prenom', TextType::class,array(
+                'label'=>'Prenom Coach',
+                'attr'=>[
+                    'placeholder'=>'Foulen....'
+                ]
+            ))
+            ->add('email', TextType::class,array(
+                'label'=>'Email',
+                'attr'=>[
+                    'placeholder'=>'Foulen.benFoulen@yahoo.fr'
+                ]
+            ))
             ->add('password')
             ->add('tel')
             ->add('adresse')
             ->add('image')
             ->add('rating')
-            ->add('date')
-        ;
+            ->add('date',DateType::class, array(
+                'widget'=>'single_text',
+                'format'=>'yyyy-MM-dd'
+            ));
     }
 
     public function configureOptions(OptionsResolver $resolver)
